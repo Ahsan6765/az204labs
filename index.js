0e5e6547afac83741eb4f2e1e36c5117e216d393
Ahsan <malikahsansultan@gmail.com>
2024-12-03 18:28:53 +0500

    initial code commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// const http = require('http');
+
+// const server = http.createServer(function(request, response) { 
+//     response.writeHead(200, { "Content-Type": "text/html" });
+//     response.end("<html><body><h1>Hello World!</h1></body></html>");
+// });
+
+// const port = process.env.PORT || 1337;
+// server.listen(port);
+
+// console.log(\`Server running at http://localhost:\${port}\`);
+
+
+
+const http = require('http');
+
+const server = http.createServer(function(request, response) { 
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("<html><body><h1>Hello World!</h1></body></html>");
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port);
+
+console.log(`Server running at http://localhost:${port}`);
